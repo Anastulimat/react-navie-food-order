63c8047429a10c4b00bcec5dac51d6d6b665cac8
Anas TULIMAT <anas.touliemat@gmail.com>
2025-07-09 22:39:28 +0200

    feat: add auth screens

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -2,4 +2,6 @@ export const appwriteConfig = {
     endpoint: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
     project: process.env.NEXT_PUBLIC_APPWRITE_PROJECT,
     platform: "com.anas.fastfood",
+    databaseId: "686ecd08000c9f84db76",
+    userCollectionId: "686ecd38002f9ebe8497",
 }
