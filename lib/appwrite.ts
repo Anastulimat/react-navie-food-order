56570a77cf474c17c04708ece25391936d51fc79
Anas TULIMAT <anas.touliemat@gmail.com>
2025-07-09 22:09:41 +0200

    feat: configure appwrite

---
+++ lib/appwrite.ts
@@ -0,0 +1,5 @@
+export const appwriteConfig = {
+    endpoint: process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT,
+    project: process.env.NEXT_PUBLIC_APPWRITE_PROJECT,
+    platform: "com.anas.fastfood",
+}
