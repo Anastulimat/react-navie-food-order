c0b3adfc65b5bc05f9978ef04feef50ea864c9d2
Anas TULIMAT <anas.touliemat@gmail.com>
2025-07-11 17:50:53 +0200

    feat: add zustand store for state management

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,10 +1,16 @@
 import {View, KeyboardAvoidingView, Platform, ScrollView, Dimensions, ImageBackground, Image} from 'react-native';
-import {Slot} from "expo-router";
+import {Redirect, Slot} from "expo-router";
 import {images} from "@/constants";
+import useAuthStore from "@/store/auth.store";
 
 // ----------------------------------------------------------------------
 
 const AuthLayout = () => {
+    const {isAuthenticated} = useAuthStore();
+
+    if (isAuthenticated) return <Redirect href="/"/>
+
+
     return (
         <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"}>
             <ScrollView
--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,10 +1,13 @@
 import {Redirect, Slot} from "expo-router";
+import useAuthStore from "@/store/auth.store";
 
 // ----------------------------------------------------------------------
 
 const TabsLayout = () => {
-    const isAuthenticated = false;
-    if(!isAuthenticated) return <Redirect href="/sign-in" />;
+    const { isAuthenticated } = useAuthStore();
+
+    if(!isAuthenticated) return <Redirect href="/sign-in" />
+
 
     return (
         <Slot />
